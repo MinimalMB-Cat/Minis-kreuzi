41f4ebc65b8db652c7258b77edac434673708ab4
MinimalMB-Cat <maxi12051992@gmail.com>
2025-12-05 12:25:43 +0100

    timeMs >0

--- src/App.tsx
+++ src/App.tsx
@@ -320,7 +320,6 @@ export default function App() {
         nickname: nick.slice(0, 20),
         timeMs: 0,        // Reload = 0 ms
       };
-      
   
       const res = await fetch('/api/highscores', {
         method: 'POST',
